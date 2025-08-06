a9cf114d3f8bf70a1578370c1d1115bb40a05d5c
Rudalph <gonsalvesrudalph@gmail.com>
2025-08-06 19:06:03 +0530

    Updates

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -15,13 +15,52 @@ export default function Page() {
 
     const handleSend = async () => {
         try {
-            const [clientPrivateKeyPem, serverPublicKeyPem] = await Promise.all([
-                fetch('/keys/client-private.pem').then(res => res.text()),
-                fetch('/keys/server-public.pem').then(res => res.text())
-            ]);
+            // const [clientPrivateKeyPem, serverPublicKeyPem] = await Promise.all([
+            //     fetch('/keys/client-private.pem').then(res => res.text()),
+            //     fetch('/keys/server-public.pem').then(res => res.text())
+            // ]);
+
+            // const clientPrivateKey = forge.pki.privateKeyFromPem(clientPrivateKeyPem);
+            // const serverPublicKey = forge.pki.publicKeyFromPem(serverPublicKeyPem);
+
+
+            
+            // ************************************************************************
+            const clientPrivateKeyPem = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCRwTGMgw9FmNgF
+vXg0oPwW/e3suyIqmQXLaFxCvaepOtOjnkG3A4JqzNR+PtGxKqBnEiGJ1he3zmam
+4Uwm0XK5whkVjr6JFx1kqu0/PFnQ9wIZcFCEqoOOHSupqBgxjFe63gbMo+IR9w1h
+PF4TE+ysQKdv6RuZEYATDCYEs301l5bGOaxgFFuvLJQSJRSTuPpWMUjn3nI+rtPW
+riyV2mpkpg+8tSvNSQwf1uibIlyzugCeg/M61M3tylyxE4o4V+cRRyRXHDTMcogZ
++lU0szKTfAH+SWKp5dU63Wo1eRdfWcKau3VlFMIF+OcNBgckWprDih0xYflTuWWB
+eyrgU3rFAgMBAAECggEAAMaDBE5bhxvtukoXGPXqTBevFl1V1zLX4bcnwSslIzSu
+6jtDT5Kd36zrTH36ZkjlxZFGdbyahQh8v1KKMdgCFj7pces/1EGNOdCMUT55noT8
+kTS/GOrspurnepBBtgcbHba6A94Iq952nG0nyvjbf3xwkkXZNxKYGIHYtJmyhtXK
+PGQNkTQfhq+OsEn+WkklrtnRgM413UJWeWSbfhYl3FMk0zcpFMQKrJTY88gBMGt4
+/cvYRLXXAnbrOI+7r8Vkyqqfghtb+VA7AONKgvg+hfRMfu1DlgQ8d95Hbx4H3bFm
+rzcZJBKxNIsdp0z/3F3A+A/eafXNpb8Bk1bPGWn/2QKBgQDIGZKdH0a5XopkGFW4
+bK2sMu4U5vckDEXS+k5TZ9Y3CG1MdGg/sWiPQ6o3yi8YA0bftWgfhSGUQrikTKQR
+34av/ko0yhJbr1Zg5zfFCYBIhBMNB8Sc+ePLfN2lIbAyGYaCSZtjIR++HdJpkx3c
+E0LZWNf7Rxv0QtL7BDrotIyDzwKBgQC6eQtvP4/prNcFgOVnse9td1SQjCCSn+f5
+t1vFlJUgaa3iWHbM1gkJX7UcCn8Jo7oL6Pv2i4Zl/N2Tkrmwgxk+3Jh2mhtpBHxZ
+SJIOQ5uL7v1t8czW27ParM7YHvJE7Mmsh54Yv231S0ehZGEyYqFMkL/6ZHwmroX/
+CfCj52v5KwKBgQC9UNyTBYVPWnAK1a88Zq5GDoWMhxzqISZqvuEeLk7REIsAW95J
+7JqlePpyADtgN7F1dFeIAthLQnkZEV3lfxsaJbeljE2kKxbJZdkw9ZVGhpbTrDbL
+4e0wl/nIA0CFlfraB7QZLLywjdyGgGcu0ZZctp7ZoWSETDj6vvzRMAkFpwKBgFiF
+M7do/iFYym4zZTOXlWhquHsx+hiAzneN4oPl5/EOGdyUUDkm9f08MNaR6QKjdaNH
+w6yjSXbK1kjC42F2qppb0AilWZO97GXZ/HPHBmUkW6xAmBP7EeMiG8Me1bWAjrBX
+ZOasoT9LsEJeS4HGrnmRUYFFCGa4fKW4i0LTjj8nAoGASoTdd7UG46aX8wmbG1ap
+Wpp1dQtMBDXZL8NY08xiAgpe6DYY3zlBP9tY0MrMO7FGhPLbOzt0Uu47wGpoNk6S
+Sx6mLvdA2otutx7CdGhnceks8JhLxLiXUodzhg8RYu9FsWpeB3vKz6fXFx6m68wF
+G4PWnw4um1hK8G3POZMQ57U=
+            -----END PRIVATE KEY-----`;
+
+            const serverPublicKeyPem = await fetch('/keys/server-public.pem').then(res => res.text());
 
             const clientPrivateKey = forge.pki.privateKeyFromPem(clientPrivateKeyPem);
             const serverPublicKey = forge.pki.publicKeyFromPem(serverPublicKeyPem);
+            // ************************************************************************
+
 
             const md = forge.md.sha256.create();
             md.update(message, 'utf8');
@@ -195,3 +234,5 @@ export default function Page() {
         </div>
     );
 }
+
+//Changes
